c1be347239380735b8dca5befb4db7db1c29ee21
Densur12 <80144786+Densur12@users.noreply.github.com>
2021-03-12 10:17:35 +0700

    Add files via upload

---
+++ I0320024_soal2_tugas2.py
@@ -0,0 +1,31 @@
+# perkenalan variabel
+
+nama = "Denny Surya Pratama"
+pendidikan = "kuliah"
+universitas = "UNS"
+beratbadan = 54.6
+tinggibadan = 172
+ukuransepatu = 41
+alamat = "Bayemharjo, Giritontro, Wonogiri, JawaTengah, Indonesia"
+tempatlahir = "Wonogiri"
+jurusan = "Teknik Industri"
+angkatan = 2020
+cita_cita = "memiliki perusahaan"
+umur = 18
+hobi = "jogging"
+golongandarah = "A"
+sosialmedia = "instagram"
+nama_akun = "densur_pr"
+nomorwa = 6282337387136
+
+#menampilkan ke layar
+
+print("Perkenalkan nama saya", nama, "saya sekarang", pendidikan)
+print("saya berkuliah di", universitas)
+print("Berat badan saya adalah", beratbadan, "Tinggi saya adalah", tinggibadan, "Dan ukuran sepatu saya adalah", ukuransepatu)
+print("saya sekarang tinggal di", alamat)
+print("saya lahir di kota", tempatlahir)
+print("saya mengambil jurusan", jurusan, "angkatan tahun", angkatan)
+print("saya bercita-cita", cita_cita, "sekarang umur saya", umur)
+print("kesukaan saya adalah", hobi, "saya memiliki golongan darah", golongandarah)
+print("saya memiliki akun sosial media", sosialmedia, "dengan nama", nama_akun, "serta nomor wa pribadi saya yaitu", nomorwa)
