1f09763ad5128296499becacd81607c9ce62bdba
Densur12 <80144786+Densur12@users.noreply.github.com>
2021-03-12 08:19:24 +0700

    Add files via upload

---
+++ I0320024_exercise2.1.py
@@ -0,0 +1,21 @@
+# Exercise 2.1
+
+#Tipe data Boolean
+print(true)
+
+#Tipe data String
+print("Ayo belajar python")
+print('Belajar Python Sangat mudah')
+
+# Tipe data Integer
+print(20)
+
+# Tipe data Float
+print(3.14)
+
+# Tipe data Hexadecimal
+number = 12
+print(hex(number))
+
+Tipe data Complex
+print(5j)
